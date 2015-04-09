0d04d75667defd3a65eaa743a806daeeccd14eed
Pete Montgomery <sumo.the.cat@gmail.com>
2015-04-09 11:42:21 +0100

    updatebyindex

--- Catalogue.Web/Controllers/Seed/SeedController.cs
+++ Catalogue.Web/Controllers/Seed/SeedController.cs
@@ -5,9 +5,11 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using Catalogue.Data.Seed;
+using Catalogue.Gemini.Model;
 using Catalogue.Web.Code;
 using Raven.Abstractions.Data;
 using Raven.Client;
+using Raven.Json.Linq;
 
 namespace Catalogue.Web.Controllers.Seed
 {
@@ -53,6 +55,78 @@ namespace Catalogue.Web.Controllers.Seed
                 {
                     Query = "Keywords:\"http://vocab.jncc.gov.uk/jncc-broad-category/Seabed Habitat Maps\""
                 });
+
+            return new HttpResponseMessage();
+        }
+
+        [HttpPost, Route("api/seed/revampkeywordsx")]
+        public HttpResponseMessage RevampKeywordsX()
+        {
+            return new HttpResponseMessage();
+        }
+
+        [HttpPost, Route("api/seed/xxx")]
+        public HttpResponseMessage Xxx()
+        {
+            WebApiApplication.DocumentStore.DatabaseCommands.UpdateByIndex("RecordIndex",
+                new IndexQuery
+                {
+                    Query = "Keywords:\"http://vocab.jncc.gov.uk/jncc-broad-category/Seabed Habitat Maps\""
+                },
+                new ScriptedPatchRequest
+                {
+                    Script = "this.Gemini.Keywords.push( { Value: 'aaa', Vocab: 'bbb' } );",
+                }
+//                new[]
+//                    {
+//                        new PatchRequest
+//                        {
+//                            Type = PatchCommandType.Set,
+//                            Name = "path",
+//                            Value = "jpjp" // RavenJObject.FromObject("New path")
+//                            Name = "gemini",
+//                            Nested = new []
+//                            {
+//                                new PatchRequest
+//                                {
+//                                    Type = PatchCommandType.Set,
+//                                    Name = "path",
+//                                    Value = RavenJObject.FromObject("New path")
+//                                }
+//                            }
+//                        }
+//                    }
+        );
+
+            return new HttpResponseMessage();
+        }
+        
+        [HttpPost, Route("api/seed/revampkeywords")]
+        public HttpResponseMessage RevampKeywords()
+        {
+            WebApiApplication.DocumentStore.DatabaseCommands.UpdateByIndex("RecordIndex",
+                new IndexQuery
+                {
+                    Query = "Keywords:\"http://vocab.jncc.gov.uk/jncc-broad-category/Seabed Habitat Maps\""
+                },
+                new []
+                    {
+                        new PatchRequest
+                        {
+                            Type = PatchCommandType.Modify,
+                            Name = "Gemini",
+                            Nested = new []
+                            {
+                                new PatchRequest
+                                {
+                                    Type = PatchCommandType.Add,
+                                    Name = "Keywords",
+                                    Value = RavenJObject.FromObject(new MetadataKeyword { Vocab = "http://vocab.jncc.gov.uk/jncc-category", Value = "Marine" })
+                                }
+                            }
+                        }
+                    },
+                new BulkOperationOptions {});
 
             return new HttpResponseMessage();
         }
